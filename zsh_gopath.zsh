a1918cb28d73c2d9d53fb49b872f44f533938145
Julian Nymark <julianymark@gmail.com>
2017-04-20 19:48:56 +0200

    no need for cd inside (retains your directory stack & history)

--- zsh_gopath.zsh
+++ zsh_gopath.zsh
@@ -1,29 +1,23 @@
 # recurse upwards, look for ./go & set GOPATH
 # (tested in zsh)
 set_gopath() {
-    # unhook chpwd (we'll be using cd inside set_gopath_recurse)
-    chpwd_functions=(${chpwd_functions[@]:#"set_gopath"})
     set_gopath_recurse "$(pwd)"
-    # rehook chpwd
-    chpwd_functions=(${chpwd_functions[@]} "set_gopath")
 }
 
 set_gopath_recurse() {
-    if [ -d "./go" ]; then
-        new_gopath "$(pwd)/go"
-        cd $1
+    if [ -d "$1/go" ]; then
+        new_gopath "$1/go"
         return
     fi;
 
-    if [ "$(pwd)" = "/" ]; then
+    if [ "$1" -ef "/" ]; then
         if [ -z $GOPATH ]; then
             echo "GOPATH NOT SET ( recursive 'cd ..' found no ./go )"
         fi;
-        cd $1
         return
     fi;
     cd ..
-    set_gopath_recurse $1
+    set_gopath_recurse "../"$1
 }
 
 new_gopath() {
